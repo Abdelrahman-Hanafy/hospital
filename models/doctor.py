db6d50919aaa07b322eb1faeccb15c2bde78fb62
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-07-13 14:42:12 +0300

    [Add] The ablility to add avail time slots for the doctor

--- models/doctor.py
+++ models/doctor.py
@@ -11,6 +11,7 @@ class doctor(models.Model):
 
     visit_ids = fields.One2many(comodel_name="hospital.patient.visit", inverse_name="doctor_id", string="Visits")
 
+    availability_ids = fields.One2many(comodel_name="doctor.availability", inverse_name="doctor_id", string="Availability")
 
     @api.model
     def create(self, vals):
@@ -19,3 +20,27 @@ class doctor(models.Model):
         result.create_employee_id = self.env['hr.employee'].search([('user_id', '=', result.id)])
 
         return result
+    
+class doctorAvailability(models.Model):
+    _name = 'doctor.availability'
+
+    name = fields.Char(string="Name", compute="_compute_name", store=True)
+    doctor_id = fields.Many2one(comodel_name="res.users", string="Doctor")
+    weekday = fields.Selection([('mon', 'Monday'), ('tue', 'Tuesday'), ('wed', 'Wednesday'), ('thu', 'Thursday'), ('fri', 'Friday'), ('sat', 'Saturday'), ('sun', 'Sunday')], string="Day")
+    start_time = fields.Float(string="Start Time")
+    end_time = fields.Float(string="End Time")
+    duration = fields.Float(string="Duration", compute="_compute_duration", inverse="_inverse_duration" ,store=True)
+
+    @api.depends('start_time', 'end_time')
+    def _compute_duration(self):
+        for rec in self:
+            rec.duration = rec.end_time - rec.start_time
+
+    def _inverse_duration(self):
+        for rec in self:
+            rec.end_time = rec.start_time + rec.duration
+
+    @api.depends('doctor_id', 'weekday')
+    def _compute_name(self):
+        for rec in self:
+            rec.name = f"{rec.doctor_id.name} - {rec.weekday}"
