51ec6d6b255087793081f8bbccaa004e34ddf6cc
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-07-13 13:03:26 +0300

    [Add] Guarantor to the views

--- models/patient.py
+++ models/patient.py
@@ -8,4 +8,5 @@ class patient(models.Model):
     visit_ids = fields.One2many(comodel_name="hospital.patient.visit", inverse_name="patient_id", string="Visits")
 
     hasGuarantor = fields.Boolean(string="Has Guarantor", default=False)
+    guarantor_id = fields.Many2one(comodel_name="res.partner", string="Guarantor", domain=[('is_company', '=', True)])
     guarantor_request_ids = fields.One2many(comodel_name="guarantor.request", inverse_name="patient_id", string="Guarantor Requests")
