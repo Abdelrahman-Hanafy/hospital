d2c764aface8b5894a57e1ccc1aef67bc7de99b8
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-07-13 14:15:29 +0300

    [Add] Invoicing to the visit

--- models/patient_visit.py
+++ models/patient_visit.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, Command
 
 
 class PatientVisit(models.Model):
@@ -14,6 +14,7 @@ class PatientVisit(models.Model):
     doctor_id = fields.Many2one('res.users', string='Doctor', domain= [('isDoctor', '=', True)])
     
     visit_type = fields.Selection([('inpatient', 'Inpatient'), ('outpatient', 'Outpatient'), ('reserve', 'Reserve')], string="Patient Type")
+    invoice_id = fields.Many2one(comodel_name='account.move', string="Invoice")
 
     @api.model
     def create(self, vals):
@@ -21,3 +22,28 @@ class PatientVisit(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('patient.visit') or 'New'
         result = super(PatientVisit, self).create(vals)
         return result
+    
+    ### Actions ###
+    def action_create_invoice(self):
+        res = self.env['account.move'].create({
+            "move_type": "out_invoice",
+            'partner_id': self.patient_id.id,
+            'invoice_date': fields.Date.today(),
+            'invoice_line_ids': [
+                Command.create(
+                    {"product_id": 1, "quantity": 1, "price_unit": 115}
+                )
+            ]
+
+        })
+        self.invoice_id = res.id
+
+
+    def action_view_invoice(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "account.move",
+            "view_mode": "form",
+            "res_id": self.invoice_id.id,
+        }
