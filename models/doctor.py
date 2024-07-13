c40eece4c8e497c23368ac86dc7c0cc72e295069
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-07-13 12:21:48 +0300

    [Fix] doctor creation errors

--- models/doctor.py
+++ models/doctor.py
@@ -6,7 +6,7 @@ class doctor(models.Model):
     isDoctor = fields.Boolean(string="Is Doctor")
     type = fields.Selection([('duty', 'Duty'), ('admission', 'Admission'), ('ER', 'Emergency'), ('consultation', 'Consultation')], string="Type")
 
-    create_employee_id = fields.Many2one(related="employee_id", string="Employee", store=True)
+    create_employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", store=True)
     employee_department_id = fields.Many2one(related="create_employee_id.department_id", string="Department", readonly=False, store=True)
 
     visit_ids = fields.One2many(comodel_name="hospital.patient.visit", inverse_name="doctor_id", string="Visits")
@@ -15,4 +15,7 @@ class doctor(models.Model):
     @api.model
     def create(self, vals):
         result = super(doctor, self).create(vals)
-        self.create_employee_id = self.env['hr.employee'].search([('user_id', '=', result.id)])
+        result.action_create_employee()
+        result.create_employee_id = self.env['hr.employee'].search([('user_id', '=', result.id)])
+
+        return result
